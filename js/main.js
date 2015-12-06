ebef43d2bf1599ba4bc3c5ccd5a71dccf3f6a5ed
Marcus Noble <marcus@marcusnoble.co.uk>
2015-12-06 18:37:30 +0000

    some style tweaks for mobile

--- js/main.js
+++ js/main.js
@@ -7,18 +7,73 @@
   recognition.lang = 'ja-JP';
 
   /* DOM elements */
-  var progressContainer = document.getElementById('progress');
-  var phraseContainer = document.getElementById('phrase');
+  var progressText = document.querySelector('#progress div');
+  var progressBar = document.querySelector('#progress progress');
+  var phraseContainer = document.querySelector('#phrase h1');
   var resultContainer = document.getElementById('result');
   var resultActionContainer = document.getElementById('resultAction');
-  var speakButton = document.querySelector('#buttons button');
+  var speakButton = document.getElementById('speakButton');
   var nextButton = document.getElementById('nextButton');
+  var playbackButton = document.getElementById('playbackButton');
+  var kanaButton = document.getElementById('kanaButton');
 
   /* Question Progress */
   var translations;
   var currentTranslation;
   var totalTranslations;
   var currentPosition = 0;
+
+  function fetchTranslations() {
+    fetch('translations.json').then(function(response) {
+      return response.json();
+    }).then(function(data) {
+      translations = data;
+      totalTranslations = translations.length;
+      nextTranslation();
+    });
+  }
+
+  function initQuestion() {
+    phraseContainer.setAttribute('class', '');
+    phraseContainer.innerText = '';
+    resultContainer.setAttribute('class', '');
+    resultContainer.innerText = '';
+    resultActionContainer.setAttribute('class', '');
+  }
+
+  function nextTranslation() {
+    initQuestion();
+    if (translations && translations.length) {
+
+      progressText.innerText = ++currentPosition + ' / ' + totalTranslations;
+      progressBar.setAttribute('max', totalTranslations);
+      progressBar.setAttribute('value', currentPosition);
+
+      currentTranslation = translations.splice(Math.floor(Math.random() * translations.length), 1)[0];
+      phraseContainer.innerText = currentTranslation.english;
+      if (typeof currentTranslation.formal !== 'undefined') {
+        var formalClass = currentTranslation.formal ? 'formal' : 'informal';
+        phraseContainer.parentNode.setAttribute('class', formalClass);
+      } else {
+        phraseContainer.parentNode.setAttribute('class', '');
+      }
+    } else {
+      alert('All done! Well done!');
+    }
+  }
+
+  playbackButton.onclick = function() {
+    utterance.text = currentTranslation.kana;
+    synth.speak(utterance);
+  };
+
+  kanaButton.onclick = function() {
+    if (phraseContainer.innerText === currentTranslation.english) {
+      phraseContainer.innerText = currentTranslation.kana;
+    } else {
+      phraseContainer.innerText = currentTranslation.english;
+    }
+  };
 
   speakButton.onclick = function() {
     if (speakButton.getAttribute('class') === 'recording') {
@@ -51,48 +106,13 @@
           resultContainer.setAttribute('class', resultClass);
           resultActionContainer.setAttribute('class', resultClass);
         });
-
-        recognition.stop();
       }
     }
   };
 
-  function fetchTranslations() {
-    fetch('translations.json').then(function(response) {
-      return response.json();
-    }).then(function(data) {
-      translations = data;
-      totalTranslations = translations.length;
-      nextTranslation();
-    });
-  }
-
-  function initQuestion() {
-    phraseContainer.setAttribute('class', '');
-    phraseContainer.innerText = '';
-    resultContainer.setAttribute('class', '');
-    resultContainer.innerText = '';
-    resultActionContainer.setAttribute('class', '');
-  }
-
-  function nextTranslation() {
-    initQuestion();
-    if (translations && translations.length) {
-
-      progressContainer.innerText = ++currentPosition + ' / ' + totalTranslations;
-
-      currentTranslation = translations.splice(Math.floor(Math.random() * translations.length), 1)[0];
-      phraseContainer.innerText = currentTranslation.english;
-      if (typeof currentTranslation.formal !== 'undefined') {
-        var formalClass = currentTranslation.formal ? 'formal' : 'informal';
-        phraseContainer.setAttribute('class', formalClass);
-      } else {
-        phraseContainer.setAttribute('class', '');
-      }
-    } else {
-      alert('All done! Well done!');
-    }
-  }
+  recognition.onerror = recognition.onend = function() {
+    speakButton.setAttribute('class', '');
+  };
 
   nextButton.onclick = nextTranslation;
 
